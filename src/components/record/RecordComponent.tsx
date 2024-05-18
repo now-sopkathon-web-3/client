5392e9796fbe45518f854510d222c843c7c2f1ca
Kjiw0n <rwd0904@naver.com>
2024-05-19 03:19:28 +0900

    feat: 이미지 업로드 api 연결 구현

--- src/components/record/RecordComponent.tsx
+++ src/components/record/RecordComponent.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Axios from 'src/api/config';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const RecordComponent = () => {
+  const BASE_URL = import.meta.env.BASE_URL;
   const [image, setImage] = useState(null);
   const [file, setFile] = useState();
   const [text, setText] = useState('');
@@ -10,46 +13,54 @@ const RecordComponent = () => {
   const [fail, setFail] = useState(false);
   const navigate = useNavigate();
 
-  const handleImageUpload = (event) => {
-    const formData = new FormData();
+  const handleImg = (event) => {
     const file = event.target.files[0];
+    setFile(file);
     if (file) {
       const reader = new FileReader();
-      reader.onload = (e) => {
-        setImage(e.target.result);
-      };
       reader.readAsDataURL(file);
+
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
     }
-    formData.append('file', file);
-    setFile(file);
+  };
+
+  const handleImageUpload = (isSuccess) => {
+    // TODO: text(상태), isSuccess 인자로 받아서 api 요청 시 같이 전달
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    Axios.post('/image/test', formData)
+      .then((result) => {
+        console.log('요청성공');
+        console.log(result);
+        setFile(null);
+      })
+      .catch((error) => {
+        console.error('요청실패');
+        console.error(error);
+      });
   };
 
   const handleInfo = (e) => {
     setText(e.target.value);
   };
 
-  const handleSuccess = (event) => {
-    console.log('success');
-    console.log('api에', { text });
-    console.log(file);
-    // 성공 api 호출?
-    navigate('/record/success');
-  };
-
-  const handleFail = (event) => {
-    console.log('fail');
-    // 실패 api 호출? 실패 페이지로 이동?
-    navigate('/record/fail');
+  const handleResult = (isSuccess: boolean) => (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    handleImageUpload(isSuccess);
+    navigate(isSuccess ? '/record/success' : '/record/fail');
   };
 
   return (
     <RecordComponentWrapper>
-      <UploadFile type="file" accept="image/gif, image/jpeg, image/png" onChange={handleImageUpload} />
+      <UploadFile type="file" accept="image/gif, image/jpeg, image/png" onChange={handleImg} />
       {image && <ImagePreview src={image} alt="Uploaded Preview" />}
-      <input onChange={handleInfo}></input>
+      <FileInfo onChange={(e) => handleInfo(e)}></FileInfo>
       <ButtonContainer>
-        <Btn onClick={handleSuccess}>성공</Btn>
-        <Btn onClick={handleFail}>실패</Btn>
+        <Btn onClick={handleResult(true)}>성공</Btn>
+        <Btn onClick={handleResult(true)}>실패</Btn>
       </ButtonContainer>
     </RecordComponentWrapper>
   );
