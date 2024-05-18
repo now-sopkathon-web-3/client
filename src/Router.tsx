23736bd098a936d8ce9df86ee6b0b7afec6ac809
Kjiw0n <rwd0904@naver.com>
2024-05-19 00:12:26 +0900

    feat: Record 페이지 관련 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,27 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Record from '@pages/record/Record';
+import SuccessPage from '@pages/record/SuccessPage';
+import FailPage from '@pages/record/FailPage';
 
 const Router = () => {
   const routerData = createBrowserRouter([
     {
       path: '/',
       element: <Home />,
+    },
+    {
+      path: '/record',
+      element: <Record />,
+    },
+    {
+      path: '/record/success',
+      element: <SuccessPage />,
+    },
+    {
+      path: '/record/fail',
+      element: <FailPage />,
     },
   ]);
 
