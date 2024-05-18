9a522a81ff5faf1d36c33bb8e2d95404d3d9f7e2
jerry <becomegoodguy@gmail.com>
2024-05-18 23:18:32 +0900

    feat: axios 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import GlobalStyle from '@styles/GlobalStyle.ts';
 import theme from '@styles/theme.ts';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,20 @@
 import Logo from '@assets/logo.svg';
 import Icon from '@components/Icon';
 import { icons } from '@styles/icons';
+import { useEffect } from 'react';
+import { getTestApi } from 'src/api';
 import styled from 'styled-components';
 
 const home = () => {
+  useEffect(() => {
+    const getTest = async () => {
+      const result = await getTestApi();
+      console.log(result);
+    };
+
+    getTest();
+  }, []);
+
   return (
     <div>
       <BigText>home</BigText>
